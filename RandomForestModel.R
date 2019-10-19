64cf2b61cdf3b1d5b70edbb28a5b648a2bb6aa2f
Rutvij <44467789+RutvijBhutaiya@users.noreply.github.com>
2019-10-19 20:30:32 +0530

    Random Forest Model Final

--- RandomForestModel.R
+++ RandomForestModel.R
@@ -14,36 +14,164 @@ TitanicCleanData = read.csv('TitanicCleanData.csv')
 
 ## Development (Study) dataSet and Validation (Test) dataset
 
-test = subset(TitanicCleanData, is.na(TitanicCleanData$Survived))
+Prediction = subset(TitanicCleanData, is.na(TitanicCleanData$Survived))
 
 train = na.omit(TitanicCleanData)
 attach(train)
 
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+# Make Ratio of 30% and 70% for test1 and train1 dataset 
+
+## Create Random variable with random numberw between 0 and 1
+
+train$random = runif(nrow(train),0,1)
+
+## Add new coloum for these new randam data
+
+train = train[order(train$random),]
+
+#Splitting the data into Development (70%) and Testing (30%) sample
+
+train1 = train[which(train$random <= 0.7),]
+test1 = train[which(train$random > 0.7),]
+
+# Remove Random dummy variable 
+
+train1 = train1[, -c(19)]
+test1 = test1[, -c(19)]
+  
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%  
 
 #Feature Selection Techniques - 
 
+attach(train1)
+
 set.seed(123)
 
-boruta.train <- Boruta(Survived ~ . , data=train, doTrace = 2)
+boruta.train <- Boruta(Survived ~ . , data=train1, doTrace = 2)
 
 ## UnImportant attruibutes - Destination, DestinationCountry, Name, PassengerId
 
-train = train[, -c(1,4, 15, 14)]
+train1 = train1[, -c(1,4, 15, 14)]
 
-str(train)
+str(train1)
 
 ## Due to Limitation and Error in Categorical variable we can not take cat class more than 53.
 
-train = train[, -c(7,8)] # Remove TicketNumber & Cabin attributes
+train1 = train1[, -c(7,8)] # Remove TicketNumber & Cabin attributes
 
 ## Build Random Forest Model 
 
 set.seed(123)
 
-train.rf = randomForest(as.factor(Survived) ~ ., data = train, 
-                        ntree = 501, mtry = 5, nodesize = 100, importance  = TRUE)
+train.rf = randomForest(as.factor(Survived) ~ ., data = train1, 
+                        ntree = 201, mtry = 6, nodesize = 20, importance  = TRUE)
 
 print(train.rf)
-plot(train.rf)
 
-  
+plot(train.rf, main="Error Rate")
+
+legend("topright", c("OOB", "0", "1"), text.col=1:6, lty=1:3, col=1:3)
+
+train.rf$err.rate
+
+train.tune = tuneRF(x = train1[, -c(1)], y = as.factor(train1$Survived),
+                    mtryStart = 6,
+                    ntreeTry = 61,
+                    stepFactor = 1.2,
+                    improve = 0.0001,
+                    trace = TRUE,
+                    plot = TRUE,
+                    nodesize = 20,
+                    doBest = TRUE,
+                    importance = TRUE
+                    ) 
+
+train.rf = randomForest(as.factor(train1$Survived) ~ ., data = train1, 
+                        ntree = 61, mtry = 5, nodesize = 20, importance  = TRUE)
+
+print(train.rf)
+
+## For Prediction class do Scoring 
+
+train1$predict.class <- predict(train.rf, train1, type = "class")
+
+View(train1)
+
+
+## Train Random Forest MOdel Performance 
+
+## Confusion Matrix 
+
+library(caret)
+library(e1071)
+
+confusionMatrix(as.factor(train1$Survived), train1$predict.class)
+
+## F1 Score
+
+precision.train1 = precision(as.factor(train1$Survived), train1$predict.class)
+# [1] 0.9915966
+
+recall.train1 = recall(as.factor(train1$Survived), train1$predict.class)
+# [1] 0.9806094
+
+train1.F1 = (2*precision.train1*recall.train1) / sum(precision.train1, recall.train1)
+# [1] 0.9860724
+
+## ROC Curve
+
+library(pROC)
+
+roc(train1$Survived, as.numeric(train1$predict.class), plot = TRUE, main = 'ROC Curve', col = 'Blue')
+
+
+
+
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% #
+
+# Apply RF Model on test1 datasets
+
+
+test1$predict.class <- predict(train.rf, test1, type = "class")
+
+View(test)
+
+## testing Random Forest MOdel Performance on test1 dataset
+
+## Confusion Matrix 
+
+library(caret)
+library(e1071)
+
+confusionMatrix(as.factor(test1$Survived), test1$predict.class)
+
+## F1 Score
+
+precision.test1 = precision(as.factor(test1$Survived), test1$predict.class)
+# [1] 0.9940828
+
+recall.test1 = recall(as.factor(test1$Survived), test1$predict.class)
+# [1] 0.9767442
+
+test1.F1 = (2*precision.test1*recall.test1) / sum(precision.test1, recall.test1)
+# [1] 0.9853372
+
+
+## ROC Curve
+
+roc(train1$Survived, as.numeric(train1$predict.class), plot = TRUE, main = 'ROC Curve for test1', col = 'darkseagreen')
+
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+## Random Forest Model applied on Unseen (Missing values) dataset - Prediction
+
+Prediction$Survived <- predict(train.rf, Prediction, type = "class")
+
+View(Prediction)
+
+write.csv(Prediction, 'Prediction Random Forest.csv')
+
+
